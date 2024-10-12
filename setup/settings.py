33aab4ed7d80b02dfff3b8b4cad1ff5d87d5e138
FREDERICO23 <adhingafredrick@gmail.com>
2024-10-12 04:45:12 +0300

    Update ALLOWED_HOSTS in settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -22,7 +22,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+#ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
